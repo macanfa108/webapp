b76638564dc6f0cd6817301e845ccde2fa6cd874
macanfa108 <810193096@qq.com>
2016-08-09 23:02:50 +0800

    初始化版本
    完成构建工具，首页初始布局

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+
+
+  })
+});
